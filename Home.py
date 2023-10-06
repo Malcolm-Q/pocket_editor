837264896cb7721b817e9e15b9ce3cf2bb38a4da
Malcolm Kew <malcolmthekew@gmail.com>
2023-10-06 16:52:02 -0700

    launcher

--- Home.py
+++ Home.py
@@ -32,22 +32,5 @@ the order that they will be joined together. Once you are satisfied with the ord
 You can also hit the delete all files button or select files to delete at the bottom of the page.\n\nThe Pipeline page 
 allows you to quickly create videos in a familiar format. EX: If you want to add reverb and distortion to a video you can 
 quickly do it in one click\n\nTODO: user created pipelines. Right now just message me if you want anything.
-\n\n
-### You may need to install ffmpeg and set the path variable.  
-Experiment first but if you get errors:
-- [Download From Here](https://ffmpeg.org/download.html)  
-- Locate FFmpeg: First, you need to know the directory where FFmpeg is installed on your system.
-
-- Open the Start menu and search for "Environment Variables" and select "Edit the system environment variables."
-
-- In the System Properties window, click the "Environment Variables..." button.
-
-- In the Environment Variables window, under the "System variables" section, scroll down until you find the "Path" variable and select it. Click the "Edit..." button.
-
-- In the Edit Environment Variable window, click the "New" button and then paste the path to the directory where FFmpeg is installed. This should be the directory containing the FFmpeg executable (e.g., ffmpeg.exe).
-
-- Click "OK" to close each of the open windows.
-
-- Open a new Command Prompt window or PowerShell window to verify that FFmpeg is now in your PATH. You can do this by running the following command: `ffmpeg -version`
-    """
+"""
 )
