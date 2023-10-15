ddaad9cdafc0f97a988b44d557fe6c622454071d
Malcolm Kew <malcolmthekew@gmail.com>
2023-10-15 14:38:54 -0700

    moviepy changes

--- pages/4_🤖_Frame_Interpolation.py
+++ pages/4_🤖_Frame_Interpolation.py
@@ -5,7 +5,7 @@ import sys
 
 import numpy as np
 import tensorflow as tf
-from moviepy.editor import VideoClip
+from moviepy.editor import concatenate_videoclips, ImageClip
 from PIL import Image
 from interp.eval import interpolator, util
 
@@ -49,10 +49,9 @@ def predict(frame1, frame2, times_to_interpolate):
     frames = list(
         util.interpolate_recursively_from_files(
             input_frames, times_to_interpolate, st.session_state.interp_model))
-    video = VideoClip(lambda t: frames[int(t * 30)], duration=len(frames) / 30)
-
-# Write the video to a file
-    video.write_videofile("out.mp4", fps=30)
+    frames = [ImageClip(frame).set_duration(1/30) for frame in frames]
+    concat_clip = concatenate_videoclips(frames, method="compose")
+    concat_clip.write_videofile("out.mp4", fps=30)
     return "out.mp4"
 
 def main():
